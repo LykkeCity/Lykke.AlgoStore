19d61908dfebdc1d1ac13c07bf479fa864203e23
Plamen Ralovski <ralovski>
2017-11-29 16:50:46 +0200

    Added placeholders for algo store service

---
+++ ExternalClient/Lykke.AlgoStore.DockerClient/ExternalClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.AlgoStore.DockerClient
+{
+    public class ExternalClient : IExternalClient
+    {
+        public async Task<bool> UploadImage(byte[] data)
+        {
+            return await Task.FromResult(true);
+        }
+    }
+}
---
+++ src/Lykke.AlgoStore.Core/Domain/Entities/DeployImageData.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lykke.AlgoStore.Core.Domain.Entities
+{
+    public class DeployImageData : BaseValidatableData
+    {
+        public string ClientId { get; set; }
+        [Required]
+        public string ImageId { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
