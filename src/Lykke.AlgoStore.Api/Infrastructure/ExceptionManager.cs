520c3750e945ea251248c28c2615b3613b72c6ae
Plamen Ralovski <ralovski>
2017-11-29 14:37:51 +0200

    Fix for exception manager if exception is aggregate

--- src/Lykke.AlgoStore.Api/Infrastructure/ExceptionManager.cs
+++ src/Lykke.AlgoStore.Api/Infrastructure/ExceptionManager.cs
@@ -17,7 +17,13 @@ namespace Lykke.AlgoStore.Infrastructure
 
         public object CreateErrorResponse(Exception ex)
         {
-            var exception = ex as AlgoStoreException;
+            Exception temp = ex;
+
+            var aggr = ex as AggregateException;
+            if (aggr != null)
+                temp = aggr.InnerExceptions[0];
+
+            var exception = temp as AlgoStoreException;
 
             if (exception == null)
                 exception = new AlgoStoreException(AlgoStoreErrorCodes.Unhandled, ex);
