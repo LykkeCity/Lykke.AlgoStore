d46fe953191e16e027df912d066a9687a8e91128
Plamen Ralovski <ralovski>
2017-12-14 09:44:58 +0200

    revert version to v1

--- src/Lykke.AlgoStore.Api/Infrastructure/Const.cs
+++ src/Lykke.AlgoStore.Api/Infrastructure/Const.cs
@@ -2,7 +2,7 @@
 {
     internal static class Const
     {
-        public const string ApiVersion = "v0.1";
+        public const string ApiVersion = "v1";
         public const string AppName = "Lykke Algo API v0.1";
     }
 }
