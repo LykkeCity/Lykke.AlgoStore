281591ca0ee3ff4156dccab4f06b87ad9e28d4b3
Plamen Ralovski <ralovski>
2017-12-19 14:16:51 +0200

    Remove not needed properies

--- src/Lykke.AlgoStore.AzureRepositories/Entities/AlgoMetaDataEntity.cs
+++ src/Lykke.AlgoStore.AzureRepositories/Entities/AlgoMetaDataEntity.cs
@@ -4,9 +4,7 @@ namespace Lykke.AlgoStore.AzureRepositories.Entities
 {
     public class AlgoMetaDataEntity : TableEntity
     {
-        public string ClientId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string TemplateId { get; set; }
     }
 }
