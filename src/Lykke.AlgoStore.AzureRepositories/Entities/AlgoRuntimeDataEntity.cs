5ed956d7481dd3bb5285e3658f80871e618c567e
Plamen Ralovski <ralovski>
2017-12-19 15:56:02 +0200

    remove unused properties from AlgoRuntimeDataEntity

--- src/Lykke.AlgoStore.AzureRepositories/Entities/AlgoRuntimeDataEntity.cs
+++ src/Lykke.AlgoStore.AzureRepositories/Entities/AlgoRuntimeDataEntity.cs
@@ -4,18 +4,7 @@ namespace Lykke.AlgoStore.AzureRepositories.Entities
 {
     public class AlgoRuntimeDataEntity : TableEntity
     {
-        public string ClientAlgoId { get; set; }
-        public string Version { get; set; }
         public int BuildImageId { get; set; }
-
-        public string AssetId { get; set; }
-        public string AssetName { get; set; }
-        public int AssetAccuracy { get; set; }
-        public int AssetInvertedAccuracy { get; set; }
-        public string AssetBaseAssetId { get; set; }
-        public string AssetQuotingAssetId { get; set; }
-
-        public string TradingAmountAssetId { get; set; }
-        public double TradingAmountAmount { get; set; }
+        public long ImageId { get; set; }
     }
 }
