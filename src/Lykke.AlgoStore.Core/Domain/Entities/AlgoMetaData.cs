ca88a68736a2568c4288d68bd5d9f8a609500880
Plamen Ralovski <ralovski>
2017-12-19 14:17:34 +0200

    Remove not needed properties

--- src/Lykke.AlgoStore.Core/Domain/Entities/AlgoMetaData.cs
+++ src/Lykke.AlgoStore.Core/Domain/Entities/AlgoMetaData.cs
@@ -10,7 +10,6 @@ namespace Lykke.AlgoStore.Core.Domain.Entities
         [Required]
         public string Name { get; set; }
         public string Description { get; set; }
-        public string TemplateId { get; set; }
         public string Date { get; set; }
         public string Status { get; set; }
 
