619b68d91973728fb5f8aac599a9c8964b78a541
Plamen Ralovski <ralovski>
2017-12-21 15:38:30 +0200

    Added repository interfaces

---
+++ src/Lykke.AlgoStore.Core/Domain/Repositories/IAlgoMetaDataReadOnlyRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Lykke.AlgoStore.Core.Domain.Entities;
+
+namespace Lykke.AlgoStore.Core.Domain.Repositories
+{
+    public interface IAlgoMetaDataReadOnlyRepository
+    {
+        Task<AlgoClientMetaData> GetAllClientAlgoMetaData(string clientId);
+        Task<AlgoClientMetaData> GetAlgoMetaData(string clientId, string algoId);
+        Task<bool> ExistsAlgoMetaData(string clientId, string algoId);
+    }
+}
---
+++ src/Lykke.AlgoStore.Core/Domain/Repositories/IAlgoMetaDataRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Lykke.AlgoStore.Core.Domain.Entities;
+
+namespace Lykke.AlgoStore.Core.Domain.Repositories
+{
+    public interface IAlgoMetaDataRepository : IAlgoMetaDataReadOnlyRepository
+    {
+        Task SaveAlgoMetaData(AlgoClientMetaData metaData);
+        Task DeleteAlgoMetaData(AlgoClientMetaData metaData);
+    }
+}
