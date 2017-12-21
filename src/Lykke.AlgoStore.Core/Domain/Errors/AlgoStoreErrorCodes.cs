b71dca9d4d9a990ed288c062895cbe2ebb094e0e
Plamen Ralovski <ralovski>
2017-12-21 14:21:45 +0200

    Added Algo Store Error Codes enum

---
+++ src/Lykke.AlgoStore.Core/Domain/Errors/AlgoStoreErrorCodes.cs
@@ -0,0 +1,21 @@
+﻿namespace Lykke.AlgoStore.Core.Domain.Errors
+{
+    public enum AlgoStoreErrorCodes
+    {
+        None = 0,
+
+        Unhandled = 1,
+        InternalError = 2,
+
+        //Data
+        RuntimeSettingsExists = 500,
+        UnableToDeleteData = 501,
+        AlgoNotFound = 502,
+        AlgoBinaryDataNotFound = 503,
+        AlgoRuntimeDataNotFound = 504,
+
+        //Validation - 1000
+        ValidationError = 1000,
+
+    }
+}
