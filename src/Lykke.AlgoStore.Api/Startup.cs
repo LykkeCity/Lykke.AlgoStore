6cc764c0b9db1140c18181d03396428f57ce7f14
Plamen Ralovski <ralovski>
2017-12-21 14:58:19 +0200

    added base entry point of application

---
+++ src/Lykke.AlgoStore.Api/Startup.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using Common.Log;
+using Lykke.AlgoStore.Api.Infrastructure;
+using Lykke.AlgoStore.Api.Infrastructure.Authentication;
+using Lykke.AlgoStore.Core.Constants;
+using Lykke.AlgoStore.Core.Settings;
+using Lykke.Common.ApiLibrary.Middleware;
+using Lykke.Common.ApiLibrary.Swagger;
+using Lykke.SettingsReader;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lykke.AlgoStore.Api
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+        public IContainer ApplicationContainer { get; private set; }
+        public IConfigurationRoot Configuration { get; }
+        public ILog Log { get; private set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.AddProfiles(typeof(AutoMapperProfile));
+            });
+
+            Mapper.AssertConfigurationIsValid();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            Environment = env;
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            try
+            {
+                services.AddMvc()
+                    .AddJsonOptions(options =>
+                    {
+                        options.SerializerSettings.ContractResolver =
+                            new Newtonsoft.Json.Serialization.DefaultContractResolver();
+                    });
+
+                services.AddSwaggerGen(options =>
+                {
+                    options.DefaultLykkeConfiguration(AlgoStoreConstants.ApiVersion, AlgoStoreConstants.AppName);
+                    options.OperationFilter<ApiKeyHeaderOperationFilter>();
+                });
+
+                services.AddLykkeAuthentication();
+
+                var appSettings = Configuration.LoadSettings<AppSettings>();
+                Log = LogManager.CreateLogWithSlack(services, appSettings);
+
+                ApplicationContainer = ContainerManager.RegisterAlgoApiModules(services, appSettings, Log);
+
+                return new AutofacServiceProvider(ApplicationContainer);
+            }
+            catch (Exception ex)
+            {
+                Log?.WriteFatalErrorAsync(nameof(Startup), nameof(ConfigureServices), "", ex).Wait();
+                throw;
+            }
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
+        {
+            try
+            {
+                app.UseCors(builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                });
+                app.Use(next => context =>
+                {
+                    context.Request.EnableRewind();
+
+                    return next(context);
+                });
+
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+
+                app.UseMiddleware<GlobalErrorHandlerMiddleware>(AlgoStoreConstants.AppName, (CreateErrorResponse)ExceptionManager.Instance.CreateErrorResponse);
+
+                app.UseAuthentication();
+
+                app.UseMvc();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(x =>
+                {
+                    x.RoutePrefix = "swagger/ui";
+                    x.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
+                app.UseStaticFiles();
+
+                appLifetime.ApplicationStarted.Register(() => StartApplication().Wait());
+                appLifetime.ApplicationStopped.Register(() => CleanUp().Wait());
+            }
+            catch (Exception ex)
+            {
+                Log?.WriteFatalErrorAsync(nameof(Startup), nameof(ConfigureServices), "", ex).Wait();
+                throw;
+            }
+        }
+
+        private async Task StartApplication()
+        {
+            try
+            {
+                await Log.WriteMonitorAsync("", $"Env: {Program.EnvInfo}", "Started");
+            }
+            catch (Exception ex)
+            {
+                await Log.WriteFatalErrorAsync(nameof(Startup), nameof(StartApplication), "", ex);
+                throw;
+            }
+        }
+
+        private async Task CleanUp()
+        {
+            try
+            {
+                if (Log != null)
+                {
+                    await Log.WriteMonitorAsync("", $"Env: {Program.EnvInfo}", "Terminating");
+                }
+
+                ApplicationContainer.Dispose();
+            }
+            catch (Exception ex)
+            {
+                if (Log != null)
+                {
+                    await Log.WriteFatalErrorAsync(nameof(Startup), nameof(CleanUp), "", ex);
+                    (Log as IDisposable)?.Dispose();
+                }
+                throw;
+            }
+        }
+    }
+}
