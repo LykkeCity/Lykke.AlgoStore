698bd1aeb150f1b0af2afbf7abf71c599b66d52a
Plamen Ralovski <ralovski>
2017-12-21 16:43:02 +0200

    added runtime repository

---
+++ src/Lykke.AlgoStore.Core/Domain/Repositories/IAlgoRuntimeDataReadOnlyRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Lykke.AlgoStore.Core.Domain.Entities;
+
+namespace Lykke.AlgoStore.Core.Domain.Repositories
+{
+    public interface IAlgoRuntimeDataReadOnlyRepository
+    {
+        Task<AlgoClientRuntimeData> GetAlgoRuntimeData(string clientId, string algoId);
+    }
+}
---
+++ src/Lykke.AlgoStore.Core/Domain/Repositories/IAlgoRuntimeDataRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Lykke.AlgoStore.Core.Domain.Entities;
+
+namespace Lykke.AlgoStore.Core.Domain.Repositories
+{
+    public interface IAlgoRuntimeDataRepository : IAlgoRuntimeDataReadOnlyRepository
+    {
+        Task SaveAlgoRuntimeData(AlgoClientRuntimeData data);
+        Task<bool> DeleteAlgoRuntimeData(string clientId, string algoId);
+    }
+}
