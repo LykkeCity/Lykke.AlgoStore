78e6f40cf04cfae0f1150b098644201093155d54
Plamen Ralovski <ralovski>
2017-12-21 16:47:18 +0200

    Cleanup and infrastructure fix

--- src/Lykke.AlgoStore.Api/Infrastructure/AutoMapperProfile.cs
+++ src/Lykke.AlgoStore.Api/Infrastructure/AutoMapperProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Lykke.AlgoStore.Api.Models;
+using Lykke.AlgoStore.Core.Domain.Entities;
 
 namespace Lykke.AlgoStore.Api.Infrastructure
 {
@@ -6,6 +8,13 @@ namespace Lykke.AlgoStore.Api.Infrastructure
     {
         public AutoMapperProfile()
         {
+            CreateMap<AlgoMetaData, AlgoMetaDataModel>()
+                    .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.AlgoId));
+            CreateMap<AlgoMetaDataModel, AlgoMetaData>()
+                .ForMember(dest => dest.AlgoId, opt => opt.MapFrom(src => src.Id));
+            CreateMap<UploadAlgoBinaryModel, UploadAlgoBinaryData>();
+
+            CreateMap<ManageImageData, ManageImageModel>().ReverseMap();
         }
     }
 }
