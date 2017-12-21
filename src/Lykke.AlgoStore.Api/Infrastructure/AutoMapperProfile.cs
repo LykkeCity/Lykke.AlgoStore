4258df64763e65d3a60530d4b902e8a75edacddd
Plamen Ralovski <ralovski>
2017-12-21 14:55:24 +0200

    Added AutoMapperProfile

---
+++ src/Lykke.AlgoStore.Api/Infrastructure/AutoMapperProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace Lykke.AlgoStore.Api.Infrastructure
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+        }
+    }
+}
