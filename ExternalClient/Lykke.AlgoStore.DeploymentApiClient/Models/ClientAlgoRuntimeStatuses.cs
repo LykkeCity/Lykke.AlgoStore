e8b21647abbed81e87701418f3af5fb2c469aa62
Plamen Ralovski <ralovski>
2017-12-21 16:35:36 +0200

    Added statuses enum

---
+++ ExternalClient/Lykke.AlgoStore.DeploymentApiClient/Models/ClientAlgoRuntimeStatuses.cs
@@ -0,0 +1,15 @@
+﻿namespace Lykke.AlgoStore.DeploymentApiClient.Models
+{
+    public enum ClientAlgoRuntimeStatuses
+    {
+        Success = 0,
+        InternalError,
+        NotFound,
+        Forbidden,
+        Unauthorized,
+        Running,
+        Stopped,
+        Paused,
+        Created
+    }
+}
