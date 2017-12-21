4d9aae03d3cdc0c485e04074044a2bffe95062ff
Plamen Ralovski <ralovski>
2017-12-21 14:23:17 +0200

    Added ILykkePrincipal

---
+++ src/Lykke.AlgoStore.Core/Identity/ILykkePrincipal.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Lykke.AlgoStore.Core.Identity
+{
+    public interface ILykkePrincipal
+    {
+        Task<ClaimsPrincipal> GetCurrent();
+        string GetToken();
+        void InvalidateCache(string token);
+    }
+}
