ee1fac46e8c854a002699f607403bd635e7da253
Plamen Ralovski <ralovski>
2017-12-21 15:36:10 +0200

    Added AlgoMetaData data classes

---
+++ src/Lykke.AlgoStore.Core/Domain/Entities/AlgoClientMetaData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Lykke.AlgoStore.Core.Domain.Entities
+{
+    public class AlgoClientMetaData
+    {
+        public string ClientId { get; set; }
+        public List<AlgoMetaData> AlgoMetaData { get; set; }
+    }
+}
