af72f4eb7530452fb5a2dd6d96e02561fc49f7cf
Plamen Ralovski <ralovski>
2017-12-21 15:43:55 +0200

    Added service interface

---
+++ src/Lykke.AlgoStore.Core/Services/IAlgoStoreClientDataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Lykke.AlgoStore.Core.Domain.Entities;
+
+namespace Lykke.AlgoStore.Core.Services
+{
+    public interface IAlgoStoreClientDataService
+    {
+        Task<AlgoClientMetaData> GetClientMetadata(string clientId);
+        Task<AlgoClientMetaData> SaveClientMetadata(string clientId, AlgoMetaData data);
+        Task SaveAlgoAsBinary(string clientId, UploadAlgoBinaryData dataModel);
+        Task DeleteMetadata(string clientId, AlgoMetaData data);
+    }
+}
