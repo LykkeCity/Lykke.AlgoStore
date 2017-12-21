6e98a61a1249aeead6d7ef5e7be3fd7c5179cded
Plamen Ralovski <ralovski>
2017-12-21 14:27:05 +0200

    Added settings related entities

---
+++ src/Lykke.AlgoStore.Core/Settings/ServiceSettings/DbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Lykke.AlgoStore.Core.Settings.ServiceSettings
+{
+    public class DbSettings
+    {
+        public string LogsConnString { get; set; }
+        public string TableStorageConnectionString { get; set; }
+    }
+}
---
+++ src/Lykke.AlgoStore.Core/Settings/ServiceSettings/ServiceSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Lykke.AlgoStore.Core.Settings.ServiceSettings
+{
+    public class ServiceSettings
+    {
+        public string SessionServiceUrl { get; set; }
+        public string DeploymentApiServiceUrl { get; set; }
+    }
+}
