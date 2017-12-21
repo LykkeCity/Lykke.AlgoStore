995c1972a835d4d0c7624019c9832e9669c85b0e
Plamen Ralovski <ralovski>
2017-12-21 16:46:31 +0200

    added management controller

---
+++ src/Lykke.AlgoStore.Api/Controllers/AlgoStoreManagementController.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using AutoMapper;
+using Lykke.AlgoStore.Api.Infrastructure.Extensions;
+using Lykke.AlgoStore.Api.Models;
+using Lykke.AlgoStore.Core.Domain.Entities;
+using Lykke.AlgoStore.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Lykke.AlgoStore.Api.Controllers
+{
+    [Authorize]
+    [Route("api/v1/management")]
+    public class AlgoStoreManagementController : Controller
+    {
+        private readonly IAlgoStoreService _service;
+
+        public AlgoStoreManagementController(IAlgoStoreService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("deploy/binary")]
+        [SwaggerOperation("DeployBinaryImage")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BaseErrorResponse), (int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> DeployBinaryImage([FromBody]ManageImageModel model)
+        {
+            var data = Mapper.Map<ManageImageData>(model);
+            data.ClientId = User.GetClientId();
+
+            var result = await _service.DeployImage(data);
+
+            return Ok(result);
+        }
+    }
+}
