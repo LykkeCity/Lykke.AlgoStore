48eddd0837dfef56add34d5251ad6aa7c41e8c2e
Plamen Ralovski <ralovski>
2017-12-21 15:42:59 +0200

    added blob model

---
+++ src/Lykke.AlgoStore.Api/Models/UploadAlgoBinaryModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Lykke.AlgoStore.Api.Models
+{
+    public class UploadAlgoBinaryModel
+    {
+        public IFormFile Data { get; set; }
+        public string AlgoId { get; set; }
+    }
+}
