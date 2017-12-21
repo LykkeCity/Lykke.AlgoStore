d75b406c1a1a8fb541caab9ff8b396c4911c6a5f
Plamen Ralovski <ralovski>
2017-12-21 14:57:35 +0200

    Added error responses

---
+++ src/Lykke.AlgoStore.Api/Models/BaseErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Lykke.AlgoStore.Api.Models
+{
+    public class BaseErrorResponse
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorDescription { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
