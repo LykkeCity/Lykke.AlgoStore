6269ef97c2e6470d5642d53972fe8f533b6d74d8
Plamen Ralovski <ralovski>
2017-12-21 14:22:06 +0200

    Added AlgoStoreException

---
+++ src/Lykke.AlgoStore.Core/Domain/Errors/AlgoStoreException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lykke.AlgoStore.Core.Domain.Errors
+{
+    public class AlgoStoreException : Exception
+    {
+        const string ErrorMessageFormat = "Code:{0}-{1} Message:{2}";
+
+        public AlgoStoreException(AlgoStoreErrorCodes errorCode) : this(errorCode, string.Empty)
+        { }
+        public AlgoStoreException(AlgoStoreErrorCodes errorCode, Exception exception) : this(errorCode, string.Empty, exception)
+        { }
+        public AlgoStoreException(AlgoStoreErrorCodes errorCode, string errorMessage) : this(errorCode, errorMessage, null)
+        { }
+
+        public AlgoStoreException(AlgoStoreErrorCodes errorCode, string errorMessage, Exception exception) :
+            base(string.Format(ErrorMessageFormat, errorCode.ToString("d"), errorCode.ToString("g"), errorMessage ?? string.Empty), exception)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage ?? string.Empty;
+        }
+
+        public AlgoStoreErrorCodes ErrorCode { get; private set; }
+        public string ErrorMessage { get; private set; }
+    }
+}
