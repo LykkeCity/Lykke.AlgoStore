428d2bcfad30b83ac7001249c79484f2b7a934b1
Plamen Ralovski <ralovski>
2017-12-21 14:58:01 +0200

    added modules

---
+++ src/Lykke.AlgoStore.Api/Modules/AlgoRepositoryModule.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Common.Log;
+using Lykke.AlgoStore.Core.Settings;
+using Lykke.SettingsReader;
+
+namespace Lykke.AlgoStore.Api.Modules
+{
+    public class AlgoRepositoryModule : Module
+    {
+        private readonly IReloadingManager<AppSettings> _settings;
+        private readonly ILog _log;
+
+        public AlgoRepositoryModule(IReloadingManager<AppSettings> settings, ILog log)
+        {
+            _settings = settings;
+            _log = log;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterInstance(_log).SingleInstance();
+
+            var reloadingDbManager = _settings.ConnectionString(x => x.AlgoApi.Db.TableStorageConnectionString);
+        }
+    }
+}
