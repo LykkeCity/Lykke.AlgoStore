7582595bef42a95dc19a77bfc87306566d3e4b79
Plamen Ralovski <ralovski>
2017-12-21 14:24:25 +0200

    Added StringExtensions

---
+++ src/Lykke.AlgoStore.Core/Utils/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lykke.AlgoStore.Core.Utils
+{
+    public static class StringExtensions
+    {
+        public const char CommaSeparator = ',';
+        public const char ColonSeparator = ':';
+        public const char SemiColonSeparator = ';';
+
+        public static List<string> SplitTrimmed(this string stringList, char separator, bool nullOrWhitespaceInputReturnsNull = false)
+        {
+            if (string.IsNullOrWhiteSpace(stringList))
+                return nullOrWhitespaceInputReturnsNull ? null : new List<string>();
+
+            return stringList
+                .TrimEnd(separator)
+                .Split(separator)
+                .AsEnumerable()
+                .Select(s => s.Trim())
+                .ToList();
+        }
+    }
+}
