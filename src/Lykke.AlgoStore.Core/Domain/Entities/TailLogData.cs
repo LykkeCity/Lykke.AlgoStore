22bd6d0f9fac27bb905a9fe276ee7402ab3890f7
Plamen Ralovski <ralovski>
2017-12-22 08:23:20 +0200

    Added TailLogData

---
+++ src/Lykke.AlgoStore.Core/Domain/Entities/TailLogData.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lykke.AlgoStore.Core.Domain.Entities
+{
+    public class TailLogData : BaseValidatableData
+    {
+        [Required]
+        public string AlgoId { get; set; }
+        [Required]
+        public string ClientId { get; set; }
+        [Required]
+        public int Tail { get; set; }
+    }
+}
