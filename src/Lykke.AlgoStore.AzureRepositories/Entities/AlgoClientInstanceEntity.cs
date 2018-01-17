5878f53e9f11fc87e68bebc7a7496f18a7d02d71
Plamen Ralovski <ralovski>
2018-01-17 16:00:13 +0200

    added instance entity

---
+++ src/Lykke.AlgoStore.AzureRepositories/Entities/AlgoClientInstanceEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Lykke.AlgoStore.AzureRepositories.Entities
+{
+    public class AlgoClientInstanceEntity : TableEntity
+    {
+        public string HftApiKey { get; set; }
+        public string AssetPair { get; set; }
+        public string TradedAsset { get; set; }
+        public double Volume { get; set; }
+        public double Margin { get; set; }
+    }
+}
