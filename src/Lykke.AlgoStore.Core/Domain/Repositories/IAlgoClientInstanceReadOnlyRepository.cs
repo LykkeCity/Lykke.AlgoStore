9631d61cbfb66e119c9eafef73b670b1a2e0f694
Plamen Ralovski <ralovski>
2018-01-17 16:01:49 +0200

    added instance data repository

---
+++ src/Lykke.AlgoStore.Core/Domain/Repositories/IAlgoClientInstanceReadOnlyRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.AlgoStore.Core.Domain.Entities;
+
+namespace Lykke.AlgoStore.Core.Domain.Repositories
+{
+    public interface IAlgoClientInstanceReadOnlyRepository
+    {
+        Task<List<AlgoClientInstanceData>> GetAllAlgoInstanceData(string clientId, string algoId);
+        Task<AlgoClientInstanceData> GetAlgoInstanceData(string clientId, string algoId, string instanceId);
+        Task<bool> ExistsAlgoInstanceData(string clientId, string algoId, string instanceId);
+    }
+}
