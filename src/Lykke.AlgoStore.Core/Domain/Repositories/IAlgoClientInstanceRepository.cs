e84139d9421d437038cf3ace9ba985a3e94b4af1
Plamen Ralovski <ralovski>
2018-01-17 16:01:49 +0200

    added instance data repository

---
+++ src/Lykke.AlgoStore.Core/Domain/Repositories/IAlgoClientInstanceRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Lykke.AlgoStore.Core.Domain.Entities;
+
+namespace Lykke.AlgoStore.Core.Domain.Repositories
+{
+    public interface IAlgoClientInstanceRepository : IAlgoClientInstanceReadOnlyRepository
+    {
+        Task SaveAlgoInstanceData(AlgoClientInstanceData data);
+        Task DeleteAlgoInstanceData(AlgoClientInstanceData metaData);
+    }
+}
