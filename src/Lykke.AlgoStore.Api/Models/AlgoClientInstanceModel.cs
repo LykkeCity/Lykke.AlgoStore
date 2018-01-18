7a1743df9f3365c70b0a956aec0e595e0129d321
Plamen Ralovski <ralovski>
2018-01-18 15:22:24 +0200

    Added AlgoClientInstanceModel

---
+++ src/Lykke.AlgoStore.Api/Models/AlgoClientInstanceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Lykke.AlgoStore.Api.Models
+{
+    public class AlgoClientInstanceModel
+    {
+        public string InstanceId { get; set; }
+        public string AlgoId { get; set; }
+        public string HftApiKey { get; set; }
+        public string AssetPair { get; set; }
+        public string TradedAsset { get; set; }
+        public double Volume { get; set; }
+        public double Margin { get; set; }
+    }
+}
