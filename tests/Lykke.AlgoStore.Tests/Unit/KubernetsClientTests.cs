07212d8d13a1c86382535d02364d4617ba7392bb
Miladin Joksic <miladin.joksic@primeholding.com>
2018-01-18 12:25:22 +0100

    Added AutoRest Kubernetes client. Created first test for that client (get namespaces)

---
+++ tests/Lykke.AlgoStore.Tests/Unit/KubernetsClientTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Lykke.AlgoStore.KubernetesClient;
+using Lykke.AlgoStore.KubernetesClient.Models;
+using NUnit.Framework;
+
+namespace Lykke.AlgoStore.Tests.Unit
+{
+    [TestFixture]
+    public class KubernetsClientTests
+    {
+        [Test]
+        public async Task GetNamespaces_ReturnSuccess()
+        {
+            var client = Given_KubernetesClient();
+            var list = await client.ListCoreV1NamespaceWithHttpMessagesAsync();
+
+            Then_Result_ShouldContainNamespaceData(list);
+        }
+
+        private static void Then_Result_ShouldContainNamespaceData(Microsoft.Rest.HttpOperationResponse<Iok8skubernetespkgapiv1NamespaceList> list)
+        {
+            Assert.IsNotNull(list);
+            Assert.IsNotNull(list.Body.Items);
+            Assert.Greater(list.Body.Items.Count, 0);
+        }
+
+        private static IKubernetes Given_KubernetesClient()
+        {
+            var client = new Kubernetes();
+            client.SetRetryPolicy(null);
+
+            return client;
+        }
+    }
+}
