751d6a4f6f2ff6181ff6d0d8eeea00371a52b182
Plamen Ralovski <ralovski>
2018-02-02 15:52:57 +0200

    MockSettings read local appsettings.Development.json if exists

--- tests/Lykke.AlgoStore.Tests/Infrastructure/SettingsMock.cs
+++ tests/Lykke.AlgoStore.Tests/Infrastructure/SettingsMock.cs
@@ -1,18 +1,59 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Linq;
+using Lykke.AlgoStore.Core.Settings;
+using Lykke.AlgoStore.Core.Settings.ServiceSettings;
 using Lykke.SettingsReader;
+using Microsoft.Extensions.Configuration;
 using Moq;
 
 namespace Lykke.AlgoStore.Tests.Infrastructure
 {
     public static class SettingsMock
     {
+        private static readonly string FileName = "appsettings.Development.json";
+
+        public static IReloadingManager<AppSettings> InitConfigurationFromFile()
+        {
+            var config = new ConfigurationBuilder()
+                    .AddEnvironmentVariables()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .Build();
+            config.Providers.First().Set("SettingsUrl", "appsettings.Development.json");
+            config.Providers.First().Set("ASPNETCORE_ENVIRONMENT", "Development");
+            return config.LoadSettings<AppSettings>();
+        }
+
+        public static IReloadingManager<AppSettings> InitMockConfiguration()
+        {
+            var reloadingMock = new Mock<IReloadingManager<AppSettings>>();
+            reloadingMock.Setup(x => x.CurrentValue).Returns
+                (
+                    new AppSettings
+                    {
+                        AlgoApi = new AlgoApiSettings
+                        {
+                            Db = new DbSettings
+                            {
+                                TableStorageConnectionString = "Mock connectionString"
+                            }
+                        }
+                    }
+                );
+            return reloadingMock.Object;
+        }
+
         public static IReloadingManager<string> GetSettings()
         {
-            var reloadingMock = new Mock<IReloadingManager<string>>();
-            reloadingMock
-                .Setup(x => x.Reload())
-                .Returns(() => Task.FromResult("DefaultEndpointsProtocol=https;AccountName=algostoredev;AccountKey=d2VaBHrf8h8t622KvLeTPGwRP4Dxz9DTPeBT9H3zmjcQprQ1e+Z6Sx9RDqJc+zKwlSO900fzYF2Dg6oUBVDe1w=="));
-            return reloadingMock.Object;
+            IReloadingManager<AppSettings> config;
+
+            if (File.Exists(FileName))
+                config = InitConfigurationFromFile();
+            else
+                config = InitMockConfiguration();
+
+            return config.ConnectionString(x => x.AlgoApi.Db.TableStorageConnectionString);
+
+
         }
     }
 }
