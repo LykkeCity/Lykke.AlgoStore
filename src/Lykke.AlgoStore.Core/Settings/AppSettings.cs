5c2654ff7323a3566eebda3ae2057dfcea0fde38
dafinka.gerdjikova <dafinka.gerdjikova@programista.pro>
2018-02-13 18:56:49 +0200

    Add Personal Data Service section in configuration settings

--- src/Lykke.AlgoStore.Core/Settings/AppSettings.cs
+++ src/Lykke.AlgoStore.Core/Settings/AppSettings.cs
@@ -1,11 +1,13 @@
 ﻿using Lykke.AlgoStore.Core.Settings.ServiceSettings;
 using Lykke.AlgoStore.Core.Settings.SlackNotifications;
+using Lykke.Service.PersonalData.Settings;
 
 namespace Lykke.AlgoStore.Core.Settings
 {
     public class AppSettings
     {
         public AlgoApiSettings AlgoApi { get; set; }
+        public PersonalDataServiceClientSettings PersonalDataServiceClient { get; set; }
         public SlackNotificationsSettings SlackNotifications { get; set; }
     }
 }
--- src/Lykke.AlgoStore.Core/Settings/ServiceSettings/ServiceSettings.cs
+++ src/Lykke.AlgoStore.Core/Settings/ServiceSettings/ServiceSettings.cs
@@ -1,13 +1,9 @@
-﻿using Lykke.Service.PersonalData.Settings;
-
-namespace Lykke.AlgoStore.Core.Settings.ServiceSettings
+﻿namespace Lykke.AlgoStore.Core.Settings.ServiceSettings
 {
     public class ServiceSettings
     {
         public string SessionServiceUrl { get; set; }
         public string DeploymentApiServiceUrl { get; set; }
         public string AssetServiceUrl { get; set; }
-        public string ClientAccountServiceUrl { get; set; }
-        public PersonalDataServiceClientSettings PersonalDataServiceClient { get; set; }
     }
 }
