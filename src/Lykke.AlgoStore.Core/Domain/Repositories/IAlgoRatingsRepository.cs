6c24cbbe65519e2cf777e373d3fc753d12945d08
Todor Ivanov <todor.ivanov@primeholding.com>
2018-03-09 14:08:25 +0200

    Rename method params

--- src/Lykke.AlgoStore.Core/Domain/Repositories/IAlgoRatingsRepository.cs
+++ src/Lykke.AlgoStore.Core/Domain/Repositories/IAlgoRatingsRepository.cs
@@ -7,7 +7,7 @@ namespace Lykke.AlgoStore.Core.Domain.Repositories
     public interface IAlgoRatingsRepository
     {
         Task<List<AlgoRatingData>> GetAlgoRatingsAsync(string algoId);
-        Task<AlgoRatingData> GetAlgoRatingForClientAsync(string clientId, string algoId);
+        Task<AlgoRatingData> GetAlgoRatingForClientAsync(string algoId, string clientId);
         Task SaveAlgoRatingAsync(AlgoRatingData data);
     }
 }
