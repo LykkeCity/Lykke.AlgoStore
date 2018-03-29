642eefd678cb33d19bd871c8cb66c7029b5bc1ff
Miladin Joksic <miladin.joksic@primeholding.com>
2018-03-29 10:19:35 +0200

    Updated algo template models package and introduced new (empty) trades controller

---
+++ src/Lykke.AlgoStore.Api/Controllers/AlgoStoreTradesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lykke.AlgoStore.Api.Controllers
+{
+    [Authorize]
+    [Route("api/v1/trades")]
+    public class AlgoStoreTradesController : Controller
+    {
+
+    }
+}
