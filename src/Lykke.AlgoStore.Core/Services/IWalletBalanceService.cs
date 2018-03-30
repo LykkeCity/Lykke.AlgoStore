a1d3219e8675b84757cd9398e49b6a0d0ff862e5
Iliyan Ivanov <iliyan.ivanov@primeholding.com>
2018-03-30 12:01:02 +0300

    add Lykke.Service.Assets.Client nuget package to Core project

--- src/Lykke.AlgoStore.Core/Services/IWalletBalanceService.cs
+++ src/Lykke.AlgoStore.Core/Services/IWalletBalanceService.cs
@@ -4,7 +4,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Lykke.Service.Balances.AutorestClient.Models;
 using Lykke.Service.Assets.Client.Models;
-using Lykke.Service.Balances.Client.ResponseModels;
 
 namespace Lykke.AlgoStore.Core.Services
 {
