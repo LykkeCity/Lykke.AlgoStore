c3d609ced09400079fe22776be86a6b4c575b23c
Miladin Joksic <miladin.joksic@primeholding.com>
2018-05-23 10:41:14 +0200

    Hide not used property in API model

--- src/Lykke.AlgoStore.Api/Models/CreateAlgoModel.cs
+++ src/Lykke.AlgoStore.Api/Models/CreateAlgoModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Lykke.AlgoStore.Api.Models
+﻿using System.Runtime.Serialization;
+
+namespace Lykke.AlgoStore.Api.Models
 {
     public class CreateAlgoModel
     {
@@ -10,6 +12,7 @@
         public string Author { get; set; }
         public string Content { get; set; } //Base64 encoded
 
+        [IgnoreDataMember]
         public string DecodedContent => System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(Content));
     }
 }
