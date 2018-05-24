6cfb549099226f621f993cc2b9d42ee53ab1abd0
Miladin Joksic <miladin.joksic@primeholding.com>
2018-05-24 12:34:44 +0200

    Updated extract metadata

--- src/Lykke.AlgoStore.Services/Validation/NuGetReferenceProvider.cs
+++ src/Lykke.AlgoStore.Services/Validation/NuGetReferenceProvider.cs
@@ -13,7 +13,7 @@ namespace Lykke.AlgoStore.Services.Validation
         private static readonly List<(string, string)> _allowedPackages = new List<(string, string)>
         {
             // Add allowed NuGet libraries here
-            ("Lykke.AlgoStore.CSharp.AlgoTemplate.Abstractions", "1.0.4")
+            ("Lykke.AlgoStore.CSharp.AlgoTemplate.Abstractions", "1.0.5")
         };
 
         private static readonly string _nuGetPackageBaseUrl = "https://api.nuget.org/v3-flatcontainer/";
