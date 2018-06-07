acaba99b01f8dc3947f123af86b75322cb0847ce
Miladin Joksic <miladin.joksic@primeholding.com>
2018-06-07 12:01:22 +0200

    Added security client injection

---
+++ src/Lykke.AlgoStore.Api/Controllers/AlgoStoreUsersController.cs
@@ -0,0 +1,54 @@
+﻿using Lykke.AlgoStore.Api.Infrastructure.Attributes;
+using Lykke.AlgoStore.Api.Infrastructure.Extensions;
+using Lykke.AlgoStore.Api.Models;
+using Lykke.AlgoStore.Core.Domain.Entities;
+using Lykke.AlgoStore.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Lykke.AlgoStore.Service.Security.Client;
+
+namespace Lykke.AlgoStore.Api.Controllers
+{
+    [Authorize]
+    [RequirePermissionAttribute]
+    [Route("api/v1/users")]
+    public class AlgoStoreUsersController: Controller
+    {
+        private readonly IUserRolesService _userRolesService;
+        private readonly ISecurityClient _securityClient;
+
+        public AlgoStoreUsersController(IUserRolesService userRolesService, ISecurityClient securityClient)
+        {
+            _userRolesService = userRolesService;
+            _securityClient = securityClient;
+        }
+
+        [HttpGet("getAllWithRoles")]
+        [SwaggerOperation("GetAllUserRoles")]
+        [ProducesResponseType(typeof(List<AlgoStoreUserData>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BaseErrorResponse), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetAllUsersWithRoles()
+        {
+            var result = await _userRolesService.GetAllUsersWithRolesAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("getByIdWithRoles")]
+        [SwaggerOperation("GetUserByIdWithRoles")]
+        [ProducesResponseType(typeof(List<AlgoStoreUserData>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BaseErrorResponse), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetUserByIdWithRoles(string clientId)
+        {
+            if (string.IsNullOrEmpty(clientId))
+                clientId = User.GetClientId();
+
+            var result = await _userRolesService.GeyUserByIdWithRoles(clientId);
+
+            return Ok(result);
+        }
+    }
+}
