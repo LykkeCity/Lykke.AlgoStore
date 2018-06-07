d7fd210ab00ce97f2c41346d9dc0bab330bc1c6b
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-06-07 16:12:30 +0300

    Get instance free (available for instance usage) client wallets

---
+++ src/Lykke.AlgoStore.Services/AlgoStoreClientsService.cs
@@ -0,0 +1,33 @@
+﻿using Lykke.AlgoStore.Core.Domain.Entities;
+using Lykke.AlgoStore.Core.Domain.Repositories;
+using Lykke.AlgoStore.Core.Enumerators;
+using Lykke.AlgoStore.Core.Services;
+using Lykke.Service.ClientAccount.Client;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lykke.AlgoStore.Services
+{
+    public class AlgoStoreClientsService : IAlgoStoreClientsService
+    {
+        private readonly IClientAccountClient _clientAccountService;
+        private readonly IAlgoInstanceRepository _clientInstanceRepository;
+
+        public AlgoStoreClientsService(IClientAccountClient clientAccountService,
+                                       IAlgoInstanceRepository clientInstanceRepository)
+        {
+            _clientAccountService = clientAccountService;
+            _clientInstanceRepository = clientInstanceRepository;
+        }
+
+        public async Task<List<ClientWalletData>> GetAvailableClientWalletsAsync(string clientId)
+        {
+            var allWallets = await _clientAccountService.GetWalletsByClientIdAsync(clientId);
+
+            var startedOrDeploying = await _clientInstanceRepository.GetInstanceWalletIdsByStatusAsync(clientId, new AlgoInstanceStatus[] {AlgoInstanceStatus.Started, AlgoInstanceStatus.Deploying});
+
+            return allWallets.Where(w => !startedOrDeploying.Contains(w.Id)).Select(w => ClientWalletData.CreateFromDto(w)).ToList();
+        }
+    }
+}
