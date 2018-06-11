95a291c7058439919ceeb21039d0185b56d8e614
Iliyan Georgiev <iliyan.georgiev@primeholding.com>
2018-06-11 16:33:55 +0300

    Change instance name endpoint to use JSON instead of raw string

--- src/Lykke.AlgoStore.Api/Controllers/AlgoInstancesController.cs
+++ src/Lykke.AlgoStore.Api/Controllers/AlgoInstancesController.cs
@@ -7,6 +7,7 @@ using Lykke.AlgoStore.Core.Domain.Entities;
 using Lykke.AlgoStore.Core.Domain.Errors;
 using Lykke.AlgoStore.Core.Services;
 using Lykke.AlgoStore.Core.Utils;
+using Lykke.AlgoStore.Core.Validation;
 using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -138,16 +139,19 @@ namespace Lykke.AlgoStore.Api.Controllers
         [SwaggerOperation("SetInstanceNameAsync")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [ProducesResponseType((int)HttpStatusCode.OK)]
-        public async Task<IActionResult> SetInstanceNameAsync(string instanceId, [FromBody] string name)
+        public async Task<IActionResult> SetInstanceNameAsync(string instanceId, [FromBody] NameModel model)
         {
             var clientId = User.GetClientId();
+
+            if (!model.ValidateData(out var exception))
+                throw exception;
 
             var data = await _algoInstancesService.GetAlgoInstanceDataAsync(clientId, instanceId);
 
             if (data.InstanceId == null)
                 return NotFound();
 
-            data.InstanceName = name;
+            data.InstanceName = model.Name;
 
             if(data.AlgoInstanceType == CSharp.AlgoTemplate.Models.Enumerators.AlgoInstanceType.Live)
                 await _algoInstancesService.SaveAlgoInstanceDataAsync(data, data.AlgoClientId);
---
+++ src/Lykke.AlgoStore.Api/Models/NameModel.cs
@@ -0,0 +1,12 @@
+﻿using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lykke.AlgoStore.Api.Models
+{
+    public class NameModel : BaseValidatableData
+    {
+        [JsonProperty("name"), Required]
+        public string Name { get; set; }
+    }
+}
