cd177e194fb700cb72d28d35c1d25de0772887e0
Miladin Joksic <miladin.joksic@primeholding.com>
2018-06-13 11:21:23 +0200

    Updated date format for returned logs

--- src/Lykke.AlgoStore.Core/Constants/AlgoStoreConstants.cs
+++ src/Lykke.AlgoStore.Core/Constants/AlgoStoreConstants.cs
@@ -11,6 +11,7 @@
         public const string DefaultDisplayMessage = "There was a problem, please contact administrator";
 
         public const string DateTimeFormat = "yyyy-MM-ddTHH:mm:ss.fffZ";
+        public const string CustomDateTimeFormat = "yyyy-MM-dd HH:mm:ss.fff";
 
         public const string AdminRoleName = "Admin";
         public const string UserRoleName = "User";
