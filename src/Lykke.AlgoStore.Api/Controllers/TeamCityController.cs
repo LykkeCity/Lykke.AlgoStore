fcb5c2a7bc1cadfb2d970cfd43b33f503995969d
Miladin Joksic <miladin.joksic@primeholding.com>
2018-06-29 15:11:38 +0200

    Added new controller that will be called from TeamCity WebHook(s)

---
+++ src/Lykke.AlgoStore.Api/Controllers/TeamCityController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Lykke.AlgoStore.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lykke.AlgoStore.Api.Controllers
+{
+    [Route("api/v1/algoInstances")]
+    public class TeamCityController : Controller
+    {
+        [HttpPost("updateBuildStatus")]
+        public async Task<IActionResult> UpdateAlgoInstanceStatus([FromBody] TeamCityWebHookResponseModel payload)
+        {
+            if (payload == default(TeamCityWebHookResponseModel))
+                return NotFound();
+
+            return Ok();
+        }
+    }
+}
---
+++ src/Lykke.AlgoStore.Api/Models/TeamCityWebHookResponseModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Lykke.AlgoStore.Api.Models
+{
+    public class TeamCityWebHookResponseModel
+    {
+        [JsonProperty("build_start_time")]
+        public DateTime BuildStartTime { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        //REMARK: This property exist ONLY for these TC build events: Successful, Fixed, Failed, Broken
+        //and does not exist for these TC build events: Started, Changes Loaded, Interrupted, Almost Completed, Responsibility Changed
+        [JsonProperty("build_finish_time")]
+        public DateTime? BuildFinishTime { get; set; }
+
+        [JsonProperty("build_event")]
+        public string BuildEvent { get; set; }
+
+        [JsonProperty("build_name")]
+        public string BuildName { get; set; }
+
+        [JsonProperty("build_status_url")]
+        public string BuildStatusUrl { get; set; }
+
+        [JsonProperty("build_number")]
+        public int BuildNumber { get; set; }
+
+        [JsonProperty("triggered_by")]
+        public string TriggeredBy { get; set; }
+
+        [JsonProperty("build_result")]
+        public string BuildResult { get; set; }
+
+        [JsonProperty("build_result_previous")]
+        public string BuildResultPrevious { get; set; }
+
+        [JsonProperty("build_result_delta")]
+        public string BuildResultDelta { get; set; }
+    }
+}
