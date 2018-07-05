53462d21a42f38b06d03b261c21456e461fd0654
Iliyan Ivanov <iliyan.ivanov@primeholding.com>
2018-07-05 11:02:27 +0300

    Validate algo namespace. Add namespace name as project setting. Update unit tests. Add new tests

--- src/Lykke.AlgoStore.Services/CodeBuildService.cs
+++ src/Lykke.AlgoStore.Services/CodeBuildService.cs
@@ -6,9 +6,16 @@ namespace Lykke.AlgoStore.Services
 {
     public class CodeBuildService : ICodeBuildService
     {
+        private readonly string AlgoNamespaceValue;
+
+        public CodeBuildService(string algoNamespaceValue)
+        {
+            AlgoNamespaceValue = algoNamespaceValue;
+        }
+
         public ICodeBuildSession StartSession(string code)
         {
-            return new CSharpCodeBuildSession(code);
+            return new CSharpCodeBuildSession(code, AlgoNamespaceValue);
         }
     }
 }
