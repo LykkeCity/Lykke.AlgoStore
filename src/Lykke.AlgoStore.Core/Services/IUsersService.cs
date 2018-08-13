4a663a31c6f74c94a46ca1e4c48ee220186c88c0
Todor Ivanov <todor.ivanov@primeholding.com>
2018-08-13 16:00:21 +0300

    Add basic entities, repositories, services and endpoints

---
+++ src/Lykke.AlgoStore.Core/Services/IUsersService.cs
@@ -0,0 +1,14 @@
+﻿using Lykke.AlgoStore.Core.Domain.Entities;
+using System.Threading.Tasks;
+using System;
+
+namespace Lykke.AlgoStore.Core.Services
+{
+    public interface IUsersService
+    {
+        Task<UserData> GetByIdAsync(string clientId);
+        Task SetGDPRConsentAsync(string clientId, bool accepted = true);
+        Task SetCookieConsentAsync(string clientId, bool accepted = true);
+        Task DeactivateAccountAsync(string clientId);
+    }
+}
---
+++ src/Lykke.AlgoStore.Services/UsersService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Lykke.AlgoStore.Core.Domain.Entities;
+using Lykke.AlgoStore.Core.Domain.Repositories;
+using Lykke.AlgoStore.Core.Services;
+
+namespace Lykke.AlgoStore.Services
+{
+    public class UsersService: IUsersService
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersService(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public Task<UserData> GetByIdAsync(string clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetCookieConsentAsync(string clientId, bool accepted = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetGDPRConsentAsync(string clientId, bool accepted = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeactivateAccountAsync(string clientId)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
