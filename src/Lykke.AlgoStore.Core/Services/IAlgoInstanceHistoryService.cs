f4220a50c71fb997eca7114ab8ab881bf55ef416
Miladin Joksic <miladin.joksic@primeholding.com>
2018-08-23 16:10:31 +0200

    Fixed asset property namings

--- src/Lykke.AlgoStore.Core/Services/IAlgoInstanceHistoryService.cs
+++ src/Lykke.AlgoStore.Core/Services/IAlgoInstanceHistoryService.cs
@@ -15,7 +15,7 @@ namespace Lykke.AlgoStore.Core.Services
         Task<IEnumerable<Candle>> GetCandlesAsync(string assetPair, CandlePriceType priceType, CandleTimeInterval timeInterval, DateTime fromMoment, DateTime toMoment, ModelStateDictionary errorsDictionary, CancellationToken ctoken);
         Task<AlgoInstanceTradeResponse> GetTradesAsync(string instanceId, string tradedAssetId, DateTime fromMoment, DateTime toMoment);
         Task<IEnumerable<FunctionChartingUpdate>> GetFunctionsAsync(string instanceId, DateTime fromMoment, DateTime toMoment, string clientId, ModelStateDictionary errorsDictionary);
-        Task<IEnumerable<QuoteChartingUpdate>> GetQuotesAsync(string instanceId, string asetPair, DateTime fromMoment, DateTime toMoment, bool? IsBuy, string clientId, ModelStateDictionary errorsDictionary);
+        Task<IEnumerable<QuoteChartingUpdate>> GetQuotesAsync(string instanceId, string assetPair, DateTime fromMoment, DateTime toMoment, bool? IsBuy, string clientId, ModelStateDictionary errorsDictionary);
         Task<string> GetAssetPairName(string assetPairId);
     }
 }
